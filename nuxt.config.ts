7625ea1ca4214abd2916c5d39897389ce802d47d
descreetStudios <descreetstudios@gmail.com>
2025-12-16 12:10:17 +0100

    Add TailwindCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
 	modules: [ 
 		"@nuxt/eslint",
 	],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	css: ["~/assets/css/main.css"],
 });
