1ad858da87c4ab23a3132c211f4bbdc8da6c2d14
descreetStudios <descreetstudios@gmail.com>
2025-12-16 11:56:05 +0100

    Add EsLint

---
+++ eslint.config.ts
@@ -0,0 +1,27 @@
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt({
+    rules: {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"],
+        "comma-dangle": ["error", "always-multiline"],
+        "indent": ["error", "tab", { "SwitchCase": 1 }],
+        "vue/html-indent": ["error", "tab"],
+        "vue/max-attributes-per-line": ["error", {
+            singleline: 1,
+            multiline: 1,
+        }],
+        "vue/html-closing-bracket-newline": ["error", {
+            singleline: "never",
+            multiline: "always",
+        }],
+        "vue/html-closing-bracket-spacing": ["error", {
+            startTag: "never",
+            endTag: "never",
+            selfClosingTag: "always",
+        }],
+        "vue/singleline-html-element-content-newline": "off",
+        "vue/multiline-html-element-content-newline": "off",
+    },
+});
