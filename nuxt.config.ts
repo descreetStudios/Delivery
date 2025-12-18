8def5a29ea1ab0f8fb41a72d025ebdf9c511791b
ImLuCz <lucabrambilla3010@gmail.com>
2025-12-18 08:32:05 +0100

    feat: add runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,16 +3,21 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-	compatibilityDate: "2025-07-15",
-	devtools: { enabled: true },
-	modules: ["@nuxt/eslint", "@nuxt/image", "nuxt-maplibre"],
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	css: ["~/assets/css/main.css"],
-	image: {
-		provider: "ipx",
-		format: ["webp"],
-	},
-	
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint", "@nuxt/image", "nuxt-maplibre"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["~/assets/css/main.css"],
+  image: {
+    provider: "ipx",
+    format: ["webp"],
+  },
+  runtimeConfig: {
+    public: {
+      apiBase: 'http://localhost:8080',
+      wsUrl: 'ws://localhost:8080/ws/locations'
+    }
+  }
 });
