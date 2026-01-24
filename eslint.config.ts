ff7c3d303f387ac8f3201387325cad8c2ade0e88
ScalierBullet63 <199808687+ScalierBullet63@users.noreply.github.com>
2026-01-24 21:32:32 +0100

    Fix Zed Editor configurations and update dependencies

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,32 +2,29 @@
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt({
-    rules: {
-        // Line endings CRLF
-        "linebreak-style": ["error", "windows"],
+	rules: {
+		"semi": ["error", "always"],
+		"quotes": ["error", "double"],
+		"comma-dangle": ["error", "always-multiline"],
+		"indent": ["error", "tab", { "SwitchCase": 1 }],
 
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"],
-        "comma-dangle": ["error", "always-multiline"],
-        "indent": ["error", "tab", { "SwitchCase": 1, "tabWidth": 4 }],
-
-        // Vue
-        "vue/html-indent": ["error", "tab", { "baseIndent": 1 }],
-        "vue/script-indent": ["error", "tab", { "baseIndent": 0 }],
-        "vue/max-attributes-per-line": ["error", {
-            singleline: 1,
-            multiline: 1,
-        }],
-        "vue/html-closing-bracket-newline": ["error", {
-            singleline: "never",
-            multiline: "always",
-        }],
-        "vue/html-closing-bracket-spacing": ["error", {
-            startTag: "never",
-            endTag: "never",
-            selfClosingTag: "always",
-        }],
-        "vue/singleline-html-element-content-newline": "off",
-        "vue/multiline-html-element-content-newline": "off",
-    },
+		// Vue
+		"vue/html-indent": ["error", "tab",  { "baseIndent": 1 }],
+		"vue/script-indent": ["error", "tab", { "baseIndent": 0 }],
+		"vue/max-attributes-per-line": ["error", {
+			singleline: 1,
+			multiline: 1,
+		}],
+		"vue/html-closing-bracket-newline": ["error", {
+			singleline: "never",
+			multiline: "always",
+		}],
+		"vue/html-closing-bracket-spacing": ["error", {
+			startTag: "never",
+			endTag: "never",
+			selfClosingTag: "always",
+		}],
+		"vue/singleline-html-element-content-newline": "off",
+		"vue/multiline-html-element-content-newline": "off",
+	},
 });
