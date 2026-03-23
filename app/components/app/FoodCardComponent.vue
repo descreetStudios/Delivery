9239824a50fa503e077e110af0a787ca42cfb7c0
descreetStudios <descreetstudios@gmail.com>
2026-03-23 09:01:45 +0100

    Add menu placeholder

--- app/components/app/FoodCardComponent.vue
+++ app/components/app/FoodCardComponent.vue
@@ -2,19 +2,27 @@
 	<div
 		class="flex flex-col justify-center items-center bg-bg-secondary-surface shadow-lg p-2 border border-border-default rounded-2xl w-full aspect-square"
 	>
-		<h1 class="text-text-primary">PIZZA</h1>
+		<h1 class="text-text-primary">{{ name }}</h1>
 		<NuxtImg
-			class="my-1 w-1/2 h-auto"
+			class="my-1 w-1/1 h-auto"
 			draggable="false"
-			src="/images/food/pizza.png"
-			format="webp"
+			:src=imgsrc
 		/>
 		<button class="bg-warning px-3 py-0.5 border border-border-default rounded-full text-center">
-			BUY
+			Compra
 		</button>
 	</div>
 </template>
 
 <script setup>
-
+defineProps({
+	name: {
+		type: String,
+		default: "Pizza",
+	},
+	imgsrc: {
+		type: String,
+		default: "",
+	},
+});
 </script>
--- app/pages/UserPage.vue
+++ app/pages/UserPage.vue
@@ -7,17 +7,53 @@
 					@select-civic="onSearchCivicSelect"
 				/>
 				<hr class="border-0.5 border-border-default w-full">
-				<h2 class="text-text-primary">Ristorante: {{ ristorante }}</h2>
-				<div class="py-0.5 pr-0.5 border border-border-default rounded-lg">
+				<h2 class="text-text-primary"><span class="font-bold">Ristorante:</span> {{ ristorante }}</h2>
+				<div
+					v-if="ristorante"
+					class="py-0.5 pr-0.5 border border-border-default rounded-lg"
+				>
 					<div class="gap-6 grid grid-cols-2 p-5 w-full max-h-132.5 overflow-y-auto scrollbar-custom">
-						<AppFoodCardComponent />
-						<AppFoodCardComponent />
-						<AppFoodCardComponent />
-						<AppFoodCardComponent />
-						<AppFoodCardComponent />
-						<AppFoodCardComponent />
-						<AppFoodCardComponent />
-						<AppFoodCardComponent />
+						<AppFoodCardComponent
+							name="Pizza margherita"
+							imgsrc="https://eu.ooni.com/cdn/shop/articles/20220211142754-margherita-9920_0483214a-7057-4277-9a3b-f2ab17c01e13.jpg?v=1737105958&width=2048"
+						/>
+						<AppFoodCardComponent
+							name="Penne alla puttanesca" 
+							imgsrc="https://articafood.it/wp-content/uploads/ambientate/penne-alla-puttanesca.webp"
+						/>
+						<AppFoodCardComponent
+							name="Trenette al pesto"
+							imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmuthcWkzYM27s3B2MySX5GyXmNkikC-6ca83VIeOyjwaBuJGgqlITRJsLLlyZjUSQC9NSXTY-pRpSQHBP3l57xT7gIXN-LrT3o810cg8T&s=10"
+						/>
+						<AppFoodCardComponent
+							name="Bavette all'astice"
+							imgsrc="https://www.sedanoallegro.it/sites/default/files/ricette/thumbnails/0_64d2fjl0_menu_astice_180412.jpg"
+						/>
+						<AppFoodCardComponent
+							name="Risotto al fumo"
+							imgsrc="https://blog.giallozafferano.it/pierobenigni/wp-content/uploads/2020/09/RAF2-scaled.jpg"
+						/>
+						
+						<AppFoodCardComponent
+							name="Braciola di maiale con patate"
+							imgsrc="https://www.burrofuso.com/wp-content/uploads/2014/07/braciole-patate-maialino.jpg"
+						/>
+						<AppFoodCardComponent
+							name="Pollo alla cacciatora"
+							imgsrc="https://www.soniaperonaci.it/wp-content/uploads/2016/10/Pollo-alla-cacciatora-496x661.jpg"
+						/>
+						<AppFoodCardComponent
+							name="Paillard di vitello"
+							imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYOFYLiwovV55rfkExsGJjKIDmlQND965tJQ&s"
+						/>
+						<AppFoodCardComponent
+							name="Salmone alla griglia con salsa al limone"
+							imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6orMdnVk6GxJjYzt5ca0arTpETlRaNuEWfg&s"
+						/>
+						<AppFoodCardComponent
+							name="Filetto al pepe verde"
+							imgsrc="https://www.ciociariaecucina.it/wp-content/uploads/2025/10/Filetto-al-Pepe-Verde.jpg"
+						/>
 					</div>
 				</div>
 				<div>
